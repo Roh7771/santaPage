87264b2ac62459648487b7102a729653ffbb4fc8
FIRST_NAME LAST_NAME <belov.rom-77@yandex.ru>
2019-12-24 09:43:13 +0300

    Third question added

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -5,10 +5,10 @@ export default function MainPage(props) {
   return (
     <div className='container d-flex flex-column justify-content-center align-items-center vh-100'>
       <h1 className='text-center font-weight-bold greating animated fadeIn delay-1s'>Привет, дорогой Друг!</h1>
-      <div className='text animated fadeIn delay-3s'>Ты просто молодец!</div>
-      <div className='text animated fadeIn delay-3s'>Твой ответ оказался верным.</div>
-      <div className='text animated fadeIn delay-6s'>Сегодня тебя ждет новая загадка.</div>
-      <div className='text animated fadeIn delay-6s'>Отгадаешь - и подарок твой!</div>
+      <div className='text animated fadeIn delay-3s'>Твой вчерашний ответ оказался неверным :(</div>
+      <div className='text animated fadeIn delay-3s'>Но не расстраивайся.</div>
+      <div className='text animated fadeIn delay-6s'>Ты все равно получишь утешительный приз.</div>
+      <div className='text animated fadeIn delay-6s'>Сегодня тебя ждет новая загадка!</div>
       <div className='text animated fadeIn delay-9s'>Готов?</div>
       <button className='btn btn-danger go-button mb-3 mt-3 animated fadeIn delay-9s' onClick={props.onStart}>Поехали!</button>
       <img alt='' src={santa} className='d-block animated fadeIn delay-1s'></img>
