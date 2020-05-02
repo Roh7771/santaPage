9e2ebc21067bbffdeb13f826d1a74344cce6c1ad
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-02 17:01:29 +0000

    Deploy Roh7771/santaPage to github.com/Roh7771/santaPage.git:gh-pages

---
+++ precache-manifest.f20f420f564f02bb249bf1de11659905.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eec602d26e4e6fff9ebf04316bbaa42e",
+    "url": "/santaPage/index.html"
+  },
+  {
+    "revision": "3cb81f09ab47477bc8ef",
+    "url": "/santaPage/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a331961d85b82369a18e",
+    "url": "/santaPage/static/css/main.4913cda5.chunk.css"
+  },
+  {
+    "revision": "3cb81f09ab47477bc8ef",
+    "url": "/santaPage/static/js/2.0c81cd6e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/santaPage/static/js/2.0c81cd6e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a331961d85b82369a18e",
+    "url": "/santaPage/static/js/main.87038ad8.chunk.js"
+  },
+  {
+    "revision": "03cd09eea6023cfdc70a",
+    "url": "/santaPage/static/js/runtime-main.374f1b98.js"
+  },
+  {
+    "revision": "f544177cf49b9c82137bf651ef7e690d",
+    "url": "/santaPage/static/media/santa.f544177c.png"
+  }
+]);
